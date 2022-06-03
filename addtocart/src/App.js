257fb4a5d212d380eaa93769dec45b565fcdbef5
shivghari <shiv.i.ghariwala@gmail.com>
2022-06-03 14:33:44 +0530

    Create React App and make a Component of Addtocart

---
+++ addtocart/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Addtocart from './Component/Addtocart';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <Addtocart />
+    </div>
+  );
+}
+
+export default App;
---
+++ addtocart/src/Component/Addtocart.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Addtocart() {
+  return (
+    <div>
+      <h1>Add to cart Page</h1>
+    </div>
+  );
+}
+
+export default Addtocart;
